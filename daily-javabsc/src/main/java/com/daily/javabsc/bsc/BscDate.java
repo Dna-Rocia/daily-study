f83bcf2033f2000e28e671a9584efdf678134458
Rocial <loyail@163.com>
2020-10-14 18:36:42 +0800

    关于Date的内容

--- daily-javabsc/src/main/java/com/daily/javabsc/bsc/BscDate.java
+++ daily-javabsc/src/main/java/com/daily/javabsc/bsc/BscDate.java
@@ -2,6 +2,7 @@ package com.daily.javabsc.bsc;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -14,17 +15,23 @@ public class BscDate {
     public static void main(String[] args) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date  nowDate = new Date();
-        Date ol = null; //new Date("2020-10-13 20:00:00");
-        try {
-            ol = dateFormat.parse("2020-10-13 20:00:00");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+//        Date ol = null; //new Date("2020-10-13 20:00:00");
+//        try {
+//            ol = dateFormat.parse("2020-10-13 20:00:00");
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
 //        assert ol != null;  //assert 断言只适用于调试，切勿使用在业务逻辑中
+        Calendar ol = Calendar.getInstance();
         if (ol != null) {
-            System.out.println(ol.before(nowDate));
-            System.out.println("____________________________");
-            System.out.println(ol.after(nowDate));
+//            System.out.println(ol.before(nowDate));
+//            System.out.println("____________________________");
+//            System.out.println(ol.after(nowDate));
+
+            System.out.println(ol.getTime().compareTo(nowDate));
+//            System.out.println("____________________________");
+//            System.out.println(ol.compareTo(nowDate));
+
         }
     }
 }
