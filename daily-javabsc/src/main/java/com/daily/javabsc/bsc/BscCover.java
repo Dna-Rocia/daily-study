271d1f778b03ab1db338c4feb67351c43af53b15
Rocial <loyail@163.com>
2020-10-20 16:02:05 +0800

    转换类型基础部分

---
+++ daily-javabsc/src/main/java/com/daily/javabsc/bsc/BscCover.java
@@ -0,0 +1,29 @@
+package com.daily.javabsc.bsc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author ROCIA
+ * @Date 2020/10/20
+ */
+public class BscCover {
+
+    public static void main(String[] args) {
+        arr2List();
+    }
+
+
+    private static void  arr2List(){
+        String s = "1,4,5,7,89";
+        String[] arr = s.split(",");
+//       List<String> list =  Arrays.asList(arr);
+//       System.out.println(list);
+        List<Long> ids = new ArrayList<>();
+        for (String id:  arr) {
+            ids.add(Long.parseLong(id));
+        }
+        System.out.println(ids);
+    }
+}
