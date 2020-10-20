99c3d87b4212f12b8dc9be0cd2e32b0bcf78cf81
Rocial <loyail@163.com>
2020-10-20 16:01:16 +0800

    http url解析

---
+++ daily-javabsc/src/main/java/com/daily/javabsc/bsc/BscHttp.java
@@ -0,0 +1,52 @@
+package com.daily.javabsc.bsc;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * @Description
+ * @Author ROCIA
+ * @Date 2020/10/20
+ */
+public class BscHttp {
+
+    public static void main(String[] args) {
+        BscHttp bscHttp = new BscHttp();
+//        String url = "https%3A%2F%2Fm.333.com%2FoldDances%3Fcallback%3DbzccaL5brMAo14LRQW_pPRtcEtWHH5X3G5GNHP5dCpPg4DSlYS-FxVvO-72MqKslEppSAl4d8WcGy_PwHhaVi7mUoLIB5peXjxa1kNRT1b6dJtFYfzW0w7heOt9_Oz7KsyfFIS_B9aD45UtlIcFQjvh2L2KBsJpszJ6ar3UeIaDzPFunaDjYQnddTK4fu5lW";
+        String url ="http://zt.333.com/html/drawing/mszh1026.html#bxjjms-mszh-w1951?bd_vid=11865472087283966895#B_vid=11865472087283966895";
+        try {
+           String s = bscHttp.analyzeGETUrlForAdmin(url);
+           System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 解析
+     * @param url
+     * @return
+     */
+    public String analyzeGETUrlForAdmin(String url)throws Exception{
+        String value = "";
+        try {
+            if (!url.contains("://")){
+                url = URLDecoder.decode(url,"UTF-8");
+            }
+            if (url.contains("?")){
+                url = url.replace("?","$");
+                String[] splitArr = url.split("\\$");
+                value = splitArr[0];
+            }else {
+                value = url;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return URLEncoder.encode(value,"UTF-8");
+    }
+
+
+
+}
