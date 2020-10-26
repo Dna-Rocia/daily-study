9822e8830ccc80bd977dc5d6c80f155a875b1e70
Rocial <loyail@163.com>
2020-10-26 13:11:43 +0800

    分页数的计算

---
+++ daily-javabsc/src/main/java/com/daily/javabsc/interview/loop/BscCalculate.java
@@ -0,0 +1,20 @@
+package com.daily.javabsc.interview.loop;
+
+/**
+ * @Description
+ * @Author ROCIA
+ * @Date 2020/10/24
+ */
+public class BscCalculate {
+
+    public static void main(String[] args) {
+       long i = 614613;
+//        long i = 11000;
+       double f =  (double)i%5000;
+        System.out.println(i / 5000 + (f > 0 ? 1 : 0));
+        System.out.println(f);
+
+    }
+
+
+}
