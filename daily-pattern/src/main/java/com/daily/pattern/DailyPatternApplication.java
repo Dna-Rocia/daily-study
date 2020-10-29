7dcb3e80439ca210be033b6407748972438f3619
Rocial <loyail@163.com>
2020-10-29 13:23:36 +0800

    设计模式

---
+++ daily-pattern/src/main/java/com/daily/pattern/DailyPatternApplication.java
@@ -0,0 +1,13 @@
+package com.daily.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyPatternApplication.class, args);
+    }
+
+}
