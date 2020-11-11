73fd00853b37655452a3cb30a50fca9c1286947b
Rocial <loyail@163.com>
2020-11-11 11:25:58 +0800

    将关于String部分的单独处理

---
+++ daily-javabsc/src/main/java/com/daily/javabsc/bsc/BscString.java
@@ -0,0 +1,28 @@
+package com.daily.javabsc.bsc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author ROCIA
+ * @Date 2020/11/11
+ */
+public class BscString {
+
+    public static void main(String[] args) {
+        BscString bscString = new BscString();
+        bscString.test();
+    }
+
+    private void test(){
+        String s = "1,2,10,3";
+        String[] sarr = s.split(",");
+        List<Integer> list = new ArrayList<>();
+        for (String str:sarr ) {
+            list.add(Integer.parseInt(str));
+        }
+        System.out.println(list.contains(1));
+    }
+
+}
