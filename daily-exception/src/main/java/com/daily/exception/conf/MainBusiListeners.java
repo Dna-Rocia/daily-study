84732913aa39f6f269b6770e6633533cf034b7eb
YaAndy <loyail@163.com>
2020-11-13 00:24:34 +0800

    优化自定义异常配置

--- daily-exception/src/main/java/com/daily/exception/conf/MainBusiListeners.java
+++ daily-exception/src/main/java/com/daily/exception/conf/MainBusiListeners.java
@@ -3,10 +3,12 @@ package com.daily.exception.conf;
 import com.daily.exception.utils.SpringContextUtil;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * 监听上下文
  */
+@Component
 public class MainBusiListeners implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
