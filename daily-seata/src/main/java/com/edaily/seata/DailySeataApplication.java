cbfe76096ce42cd5c92600130b8fe612f18c7665
Rocial <loyail@163.com>
2020-11-23 11:48:49 +0800

    新增一个seata的子项目

---
+++ daily-seata/src/main/java/com/edaily/seata/DailySeataApplication.java
@@ -0,0 +1,13 @@
+package com.edaily.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySeataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailySeataApplication.class, args);
+    }
+
+}
