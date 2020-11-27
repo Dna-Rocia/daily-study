b741a8323a2f351ca652dd4031081e2e8e5d71fe
Rocial <loyail@163.com>
2020-11-27 11:45:36 +0800

    整合paypal项目

---
+++ daily-common/src/main/java/com/daily/common/enums/PaypalPaymentMethod.java
@@ -0,0 +1,10 @@
+package com.daily.common.enums;
+
+/**
+ * @author Loyaill
+ * @description :
+ * @CreateTime 2018-07-31-14:22
+ */
+public enum PaypalPaymentMethod {
+	credit_card, paypal
+}
---
+++ daily-pay/src/main/java/com/daily/pay/service/PaypalService.java
@@ -0,0 +1,26 @@
+package com.daily.pay.service;
+
+import com.daily.common.enums.PaypalPaymentIntent;
+import com.daily.common.enums.PaypalPaymentMethod;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+
+/**
+ * @author Loyaill
+ * @description :
+ * @CreateTime 2018-07-31-14:24
+ */
+public interface PaypalService {
+
+	 Payment createPayment(
+			Double total,
+			String currency,
+			PaypalPaymentMethod method,
+			PaypalPaymentIntent intent,
+			String description,
+			String cancelUrl,
+			String successUrl) throws PayPalRESTException;
+
+
+	 Payment executePayment(String paymentId, String payerId) throws PayPalRESTException;
+}
